2b081f497bf9690eb9bbf2fb37631366365099c2
Amir <a.h.matloubi@gmail.com>
2024-09-12 15:24:49 +0330

    Initial Commit

---
+++ ProtocolOrientedNetworkLayer/ProtocolOrientedNetworkLayer.h
@@ -0,0 +1,18 @@
+//
+//  ProtocolOrientedNetworkLayer.h
+//  ProtocolOrientedNetworkLayer
+//
+//  Created by Amir Hossein on 12/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProtocolOrientedNetworkLayer.
+FOUNDATION_EXPORT double ProtocolOrientedNetworkLayerVersionNumber;
+
+//! Project version string for ProtocolOrientedNetworkLayer.
+FOUNDATION_EXPORT const unsigned char ProtocolOrientedNetworkLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProtocolOrientedNetworkLayer/PublicHeader.h>
+
+
