c70d8da67f1c63c602427a53e0a12b360752dd4d
Amir <a.h.matloubi@gmail.com>
2024-09-15 18:58:33 +0330

    add Networkable

---
+++ Networkable/Networkable/Networkable.h
@@ -0,0 +1,18 @@
+//
+//  Networkable.h
+//  Networkable
+//
+//  Created by Amir Hossein on 15/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Networkable.
+FOUNDATION_EXPORT double NetworkableVersionNumber;
+
+//! Project version string for Networkable.
+FOUNDATION_EXPORT const unsigned char NetworkableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networkable/PublicHeader.h>
+
+
---
+++ Networkable/Networkable/Networkable.swift
@@ -0,0 +1,8 @@
+//
+//  Networkable.swift
+//  Networkable
+//
+//  Created by Amir Hossein on 15/09/2024.
+//
+
+import Foundation
